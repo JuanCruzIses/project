49f4c90476b37da7ad185e56c6f0081d562bcab9
JuanCruzIses <juancruzises@gmail.com>
2023-08-15 15:53:35 -0300

    Commit number one million

--- pages/_app.js
+++ pages/_app.js
@@ -11,14 +11,14 @@ export default function App({ Component, pageProps }) {
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </Head>
 
-    <Script 
+    {/* <Script 
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
       crossOrigin="anonymous"
       onLoad={() =>
         console.log(`script loaded correctly, window.FB has been populated`)
       }
-    />
+    /> */}
 
     <Component {...pageProps} />
   </>
