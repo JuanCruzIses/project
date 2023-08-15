42c3f58420d55bb014b1d7cf47c75e6f84e3a479
JuanCruzIses <juancruzises@gmail.com>
2023-08-15 15:14:54 -0300

    .

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ export default function App({ Component, pageProps }) {
     <Script 
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
       integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+      crossOrigin="anonymous"
       onLoad={() =>
         console.log(`script loaded correctly, window.FB has been populated`)
       }
