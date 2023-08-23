0fb06c8aa9749638e2fab3c6c523c6e59349175b
JuanCruzIses <juancruzises@gmail.com>
2023-08-23 15:47:00 -0300

    Trying fix

--- pages/productos/jardin/index.js
+++ pages/productos/jardin/index.js
@@ -42,7 +42,7 @@ export default function Jardin(){
             </Head>
             <div className={styles.containerMainImage}>
                 <img src="/images/jardin.jpg" className="d-block w-100" alt="..."></img>
-                <div className={stylesIndex.shadow}></div>
+                <div className={styles.shadow}></div>
                 <h5 className={styles.imgWord}>Jardín</h5>
             </div>
             <h2 id="referenceToNav" className={stylesIndex.slogan}>Crea un paraíso al aire libre con nuestros muebles de jardín</h2>
