8e627194daf7e33d5107efef1b5bc602a851c401
JuanCruzIses <juancruzises@gmail.com>
2023-09-25 16:00:54 -0300

    Upgrades in image render

--- components/CarouselProductDetail.jsx
+++ components/CarouselProductDetail.jsx
@@ -11,14 +11,18 @@ export default function Carousel(props){
                 <img src={`/images/${productDetail.imagen1}`} className="d-block w-100" alt="..."></img>
                 <div className={styles.shadow}></div>
             </div>
-            <div className="carousel-item">
-                <img src={`/images/${productDetail.imagen2}`} className="d-block w-100" alt="..."></img>
-                <div className={styles.shadow}></div>
-            </div>
-            <div className="carousel-item">
-                <img src={`/images/${productDetail.imagen3}`} className="d-block w-100" alt="..."></img>
-                <div className={styles.shadow}></div>
-            </div>
+            {productDetail.imagen2 != undefined && 
+                <div className="carousel-item">
+                    <img src={`/images/${productDetail.imagen2}`} className="d-block w-100" alt="..."></img>
+                    <div className={styles.shadow}></div>
+                </div>
+            }
+            {productDetail.imagen3 != undefined && 
+                <div className="carousel-item">
+                    <img src={`/images/${productDetail.imagen3}`} className="d-block w-100" alt="..."></img>
+                    <div className={styles.shadow}></div>
+                </div>
+            }
         </div>
         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
             <span className="carousel-control-prev-icon" aria-hidden="true"></span>
